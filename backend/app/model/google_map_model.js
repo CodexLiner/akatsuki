59a446e1389f6926598b428068d5c1ca691e1266
Bhavik kothari <kotharibhavik23@gmail.com>
2022-11-04 23:01:08 +0530

    initial psuh

---
+++ backend/app/model/google_map_model.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+
+const GoogleMarkerSchema = mongoose.Schema(
+    {
+        markers: [
+            {
+                lat: {
+                    type: Location().lat,
+                    required: true
+                },
+                long: {
+                    type: Location().long,
+                    required: true
+                },
+                uid:{
+                    type: String,
+                    required: true,
+                },
+                time:{
+                    type: Date,
+                    required: true
+
+                }
+
+            }
+        ]
+    }
+);
+
+module.exports = mongoose.model('Google_Marker_Schema', GoogleMarkerSchema);
