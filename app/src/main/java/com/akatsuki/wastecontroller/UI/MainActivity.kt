92dbe00c4e125908fae98ab7518911b99330b2d0
Prateek Shah <shahprateek02@gmail.com>
2022-11-04 15:06:28 +0530

    Login and Signup UI completed

--- app/src/main/java/com/akatsuki/wastecontroller/UI/MainActivity.kt
+++ app/src/main/java/com/akatsuki/wastecontroller/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.akatsuki.wastecontroller
+package com.akatsuki.wastecontroller.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.akatsuki.wastecontroller.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
