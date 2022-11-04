0fccd91f1c38749a5f9991f7bfe549808e79ea76
Prateek Shah <shahprateek02@gmail.com>
2022-11-05 00:40:51 +0530

    added schema

---
+++ backend/app/model/general_collection.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose');
+
+const GeneralSchema = mongoose.Schema(
+    {
+
+        title :{
+            type: String,
+            required: true,
+        },
+        markers: [
+            {
+                username:{
+                    type: String,
+                    required: true,
+                },
+                description:{
+                    type: String,
+                    required: true
+                }
+                time:{
+                    type: Date,
+                    default: Date.now
+
+                }
+            }
+        ]
+    }
+);
+
+module.exports = mongoose.model('General_Schema', GeneralSchema);
